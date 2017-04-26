505253ce1eb2c9e0d0f17239e74cf5dbfde12c30
lanbing222 <lanbing222zhu@126.com>
2017-04-26 15:35:52 +0800

    atm

---
+++ src/test.py
@@ -0,0 +1,3 @@
+def hello(name):
+    print "hello %s" % name
+hello("ralap")
---
+++ test.py
@@ -0,0 +1,17 @@
+import os
+import sys
+dict_list={}
+with open("src/name.txt","r") as f:
+    name_list=f.readlines()
+    for name in name_list:
+        wo=name.split()[0]
+        dict_list[wo]=name.split()[1]
+f.close()
+print dict_list
+# with open("src/name.txt",'a') as f:
+#     f.writelines(name_list)
+# user_name=raw_input("Please input your name:")
+# if user_name in name_list:
+#     user_passwd=raw_input("password:")
+#     if user_passwd.split() == name_list[user_name][0]:
+#         print "welcome "
